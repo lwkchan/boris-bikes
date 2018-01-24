e85d3d5454921dc349c0421ea115c479544853a6
Laura Chan <laurachan205@gmail.com>
2018-01-24 16:54:11 +0000

    Refactor docking_station.rb with Single Responsibility Method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,22 @@ class DockingStation
   end
 
   def release_bike
-    raise("No bikes available") if @bikes.empty?
+    raise("No bikes available") if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise ("Docking station full") if @bikes.length >= 20
+    raise ("Docking station full") if full?
     @bikes.push(bike)
+  end
+
+  private
+  def full?
+    @bikes.length >= 20? true : false
+  end
+
+  def empty?
+    @bikes.empty? ? true : false
   end
 
 end
