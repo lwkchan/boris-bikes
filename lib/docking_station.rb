0aed42df1be0a9ec473ad6afdf26e32b246fc793
Stephen Tilley <stephentilley@gmail.com>
2018-01-24 12:09:50 +0000

    release_bike now releases a working bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,9 @@
+require_relative 'bike'
+
 class DockingStation
 
 def release_bike
+  Bike.new
 end
 
-
 end
-
-dock = DockingStation.new
-dock.release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe DockingStation do
   describe 'when called' do
     it {is_expected.to respond_to(:release_bike)}
   end
+
+    it "release_bike creates a new bike object" do
+      expect(subject.release_bike).to be_instance_of(Bike)
+    end
+
+    it "When new bike object created checks the bike is working" do
+      expect(subject.release_bike).to be_working
+    end
 end
